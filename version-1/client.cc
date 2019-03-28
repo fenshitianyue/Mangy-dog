6898cbefd378842904efee150cd8093daaeddb15
fenshitianyue <abzanda@163.com>
2019-03-28 17:30:16 +0800

    update code, can run

--- version-1/client.cc
+++ version-1/client.cc
@@ -15,7 +15,7 @@ void Client::Start(){
     perror("connect");
     exit(EXIT_FAILURE);
   }
-  //3.Create pipe. pipe_fd[0] -> father process read | pipe_fd[1] -> son process write
+  //3.Create pipe. pipe_fd[0] -> father process read | pipe_fd[1] -> child process write
   int pipe_fd[2];
   if(pipe(pipe_fd) < 0) { perror("pipe"); exit(EXIT_FAILURE); }
   //4.Create Epoll
@@ -27,6 +27,58 @@ void Client::Start(){
   addfd(epfd, pipe_fd[0], true);
   /* Server connection successful */
   
+  //Indicates whether the client is working properly
+  bool IsClientWork = true;
+
+  //chat message buffer
+  char message[BUF_SIZE] = {0};
+  int pid = fork();
+  CheckPrint(pid, "fork");
+  if(0 == pid){ //child process
+    close(pipe_fd[0]);
+    std::cout << "Input 'exit' to exit the chat room" << std::endl;
+    while(IsClientWork){
+      fgets(message, BUF_SIZE, stdin);
+      //client input 'exit', exit chat room
+      if(0 == strncasecmp(message, "exit", strlen("exit"))){
+        IsClientWork = false;
+      }else{ //Otherwise the child process writes the information to the pipe
+        CheckPrint(write(pipe_fd[1], message, strlen(message) - 1), "write");
+      }
+    }
+  }else { //father process
+    close(pipe_fd[1]);
+    while(IsClientWork){
+      int epoll_events_count = epoll_wait(epfd, events, 2, -1);
+      for(int i = 0; i < epoll_events_count; ++i){
+        if(events[i].data.fd == sock){ //The server sent the message
+          int ret = recv(sock, message, BUF_SIZE, 0); 
+          CheckPrint(ret, "recv");
+          if(0 == ret){
+            std::cout << "Server close connection: " << sock << std::endl;
+            close(sock);
+            IsClientWork = false;
+          }else{
+            std::cout << message << std::endl;
+          }
+        }else{//The child writes to event and the parent processes and sends it to the server
+          int ret = read(events[i].data.fd, message, BUF_SIZE);
+          CheckPrint(ret, "read");
+          if(0 == ret){
+            IsClientWork = false;
+          }else{
+            send(sock, message, BUF_SIZE, 0);
+          }
+        }
+      }//end for
+    }//end while
+  }
+  if(pid){
+    close(pipe_fd[0]);
+    close(sock);
+  }else{
+    close(pipe_fd[1]); 
+  }
 }
 
   
